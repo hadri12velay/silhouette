4b15f4f571e8c50d0373873dc17c5fad38a04196
hadri12velay <hadri1.2velay@gmail.com>
2023-04-23 17:11:38 +0100

    nothing don't look

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,6 +15,7 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
 
   function getMessages() {
+    console.log("getting messages");
     setLoading(true);
     try {
       const q = query(db, orderBy("timestamp", "desc"));
